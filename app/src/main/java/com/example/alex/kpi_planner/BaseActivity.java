c9ba008c9d240f86de7a47e9199645c162113944
Alex Ustymenko <ustsonder@gmail.com>
2017-12-11 00:24:11 +0200

    Add menu and 3 fragments

---
+++ app/src/main/java/com/example/alex/kpi_planner/BaseActivity.java
@@ -0,0 +1,44 @@
+package com.example.alex.kpi_planner;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class BaseActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragmentTransaction.replace(R.id.content, new ScheduleFragment()).commit();
+                    return true;
+                case R.id.navigation_dashboard:
+                    fragmentTransaction.replace(R.id.content, new SubjectsFragment()).commit();
+                    return true;
+                case R.id.navigation_notifications:
+                    fragmentTransaction.replace(R.id.content, new TeachersFragment()).commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
