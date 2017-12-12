881bbf0dd290297873eee67f136ea9e1a35e473f
Saniok126 <saniok126@gmail.com>
2017-12-12 23:54:09 +0200

    БД

--- app/src/main/java/com/example/alex/kpi_planner/BaseActivity.java
+++ app/src/main/java/com/example/alex/kpi_planner/BaseActivity.java
@@ -1,15 +1,24 @@
 package com.example.alex.kpi_planner;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class BaseActivity extends AppCompatActivity {
+
+    DBHelper dbHelper;
+    EditText etName,etNumb,etWeek;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -41,9 +50,80 @@ public class BaseActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        String groupN = "ИС-42";
-        groupN = getIntent().getExtras().getString("userGroup");
+
+
+
+
+        dbHelper = new DBHelper(this);
+    }
+
+    public void addClick(View view) {
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+        String mondey = "Mondey";
+        String tuesday = "Tuesday";
+        String wednesday = "Wednesday";
+        String thursday = "Thursday";
+        String friday = "Friday";
+        String saturday = "Saturday";
+        String week1 = "1";
+        String week2 = "2";
+        String number = "1";
+        long newRowId;
+        String [] days = {mondey,tuesday,wednesday,thursday,friday,saturday};
+
+       for (int i=0;i<days.length;i++)
+       {
+           contentValues.put(DBHelper.DAY_NUMBER,i+1);
+           contentValues.put(DBHelper.DAY_NAME,days[i]);
+           contentValues.put(DBHelper.DAY_WEEK,week2);
+           newRowId = database.insert(DBHelper.TABLE_DAY,null,contentValues);
+           if (newRowId == -1)
+           {
+               Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show();
+           }
+           else
+           {
+               Toast.makeText(this,"Добавлено: " + newRowId,Toast.LENGTH_SHORT).show();
+           }
+       }
+
+
+        dbHelper.close();
+    }
+
+    public void readClick(View view) {
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        database.delete(DBHelper.TABLE_DAY,null,null);
+         //database.execSQL(String.format("drop table if exists %s", DBHelper.TABLE_BUILDING));
+         //database.execSQL(String.format("drop table if exists %s", DBHelper.TABLE_ROOM));
+         //database.execSQL(String.format("drop table if exists %s", DBHelper.TABLE_LESSON));database.execSQL(String.format("drop table if exists %s", DBHelper.TABLE_DAY));
+         //database.execSQL(String.format("drop table if exists %s", DBHelper.TABLE_TEACHERS));
+         //database.execSQL(String.format("drop table if exists %s", DBHelper.TABLE_DISCIPLINE));
+         //database.execSQL(String.format("drop table if exists %s", DBHelper.TABLE_GROUP_DISC_RELATIONS));
+         //database.execSQL(String.format("drop table if exists %s", DBHelper.TABLE_TABLING));
+         //database.execSQL(String.format("drop table if exists %s", DBHelper.TABLE_TEACH_DISC_RELATIONS));
+         //database.execSQL(String.format("drop table if exists %s", DBHelper.TABLE_GROUP));
+
+     //  dbHelper.onCreate(database);
+        Toast.makeText(this,"БД удалена: ",Toast.LENGTH_SHORT).show();
 
     }
 
+    public void checkClick(View view) {
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+
+        String[] projection = {
+                DBHelper.DAY_ID,
+                DBHelper.DAY_NAME,
+                DBHelper.DAY_WEEK,
+                DBHelper.DAY_NUMBER};
+
+
+
+
+        dbHelper.close();
+    }
 }
--- app/src/main/java/com/example/alex/kpi_planner/DBHelper.java
+++ app/src/main/java/com/example/alex/kpi_planner/DBHelper.java
@@ -12,14 +12,61 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "groupsDB";
-    public static final String TABLE_GROUPS = "groups";
 
-    public static final String KEY_ID = "_1";
-    public static final String KEY_NAME = "name";
-    public static final String KEY_MAIL = "mail";
+    public static final String TABLE_TEACHERS = "Teacher";
+    public static final String TEACHERS_ID = "_id";
+    public static final String TEACHERS_NAME = "Name";
+    public static final String TEACHERS_SHORT = "Short_name";
+    public static final String TEACHERS_DEGREE = "Degree";
 
+    public static final String TABLE_TEACH_DISC_RELATIONS = "Teach_Disc_relations";
+    public static final String TDR_ID = "_id";
+    public static final String TDR_TEACHERS_ID = "Teacher_id";
+    public static final String TDR_DISCIPLINE_ID = "Discipline_id";
 
+    public static final String TABLE_GROUP = "class";
+    public static final String GROUP_ID = "_id";
+    public static final String GROUP_NAME = "Name";
 
+    public static final String TABLE_DAY = "Day";
+    public static final String DAY_ID = "_id";
+    public static final String DAY_NUMBER = "Number";
+    public static final String DAY_NAME = "Name";
+    public static final String DAY_WEEK = "Week";
+
+    public static final String TABLE_TABLING = "Tabling";
+    public static final String TABLING_ID = "_id";
+    public static final String TABLING_DAY_ID = "Day_id";
+    public static final String TABLING_DISC_ID = "Discipline_id";
+    public static final String TABLING_LESSON_ID = "Lesson_id";
+    public static final String TABLING_ROOM_ID = "Room_id";
+    public static final String TABLING_TYPE = "Type";
+
+    public static final String TABLE_GROUP_DISC_RELATIONS = "Group_Disc_relations";
+    public static final String GDR_ID = "_id";
+    public static final String GDR_GROUP_ID = "Group_id";
+    public static final String GDR_DISC_ID = "Discipline_id";
+
+    public static final String TABLE_DISCIPLINE = "DIscipline";
+    public static final String DISC_ID = "_id";
+    public static final String DISC_NAME = "Name";
+    public static final String DISC_FULL_NAME = "Full_name";
+
+    public static final String TABLE_LESSON = "Lesson";
+    public static final String LESSON_ID = "_id";
+    public static final String LESSON_START_TIME = "Start_time";
+    public static final String LESSON_END_TIME = "End_time";
+
+    public static final String TABLE_ROOM = "Room";
+    public static final String ROOM_ID = "_id";
+    public static final String ROOM_NAME = "Name";
+    public static final String ROOM_BUILD_ID = "Building_id";
+
+    public static final String TABLE_BUILDING = "Building";
+    public static final String BUILD_ID = "_id";
+    public static final String BUILD_NAME = "Name";
+    public static final String BUILD_LATITUDE = "Latitude";
+    public static final String BUILD_LONGITUDE = "Longitude";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -27,14 +74,30 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_GROUPS + "(" + KEY_ID +
-         "integer primary key," + KEY_NAME + " text," + KEY_MAIL + " text" + ")");
-
+        db.execSQL(String.format("create table %s (%s integer primary key, %s text, %s text, %s text)", TABLE_BUILDING, BUILD_ID, BUILD_NAME, BUILD_LATITUDE, BUILD_LONGITUDE));
+        db.execSQL(String.format("create table %s (%s integer primary key, %s text, %s text)", TABLE_ROOM, ROOM_ID, ROOM_NAME, ROOM_BUILD_ID));
+        db.execSQL(String.format("create table %s (%s integer primary key, %s text, %s text)", TABLE_LESSON, LESSON_ID, LESSON_START_TIME, LESSON_END_TIME));
+        db.execSQL(String.format("create table %s (%s integer primary key, %s text, %s text, %s text)", TABLE_DAY, DAY_ID, DAY_NAME, DAY_NUMBER, DAY_WEEK));
+        db.execSQL(String.format("create table %s (%s integer primary key, %s text, %s text, %s text)", TABLE_TEACHERS, TEACHERS_ID, TEACHERS_NAME, TEACHERS_SHORT, TEACHERS_DEGREE));
+        db.execSQL(String.format("create table %s (%s integer primary key, %s text, %s text)", TABLE_DISCIPLINE, DISC_ID, DISC_NAME, DISC_FULL_NAME));
+        db.execSQL(String.format("create table %s (%s integer primary key, %s text, %s text)", TABLE_GROUP_DISC_RELATIONS, GDR_ID, GDR_GROUP_ID, GDR_DISC_ID));
+        db.execSQL(String.format("create table %s (%s integer primary key, %s text, %s text, %s text, %s text, %s text)", TABLE_TABLING, TABLING_ID, TABLING_DAY_ID, TABLING_DISC_ID, TABLING_LESSON_ID,TABLING_ROOM_ID,TABLING_TYPE));
+        db.execSQL(String.format("create table %s (%s integer primary key, %s text, %s text)", TABLE_TEACH_DISC_RELATIONS, TDR_ID, TDR_TEACHERS_ID, TDR_DISCIPLINE_ID));
+        db.execSQL(String.format("create table %s (%s integer primary key, %s text)", TABLE_GROUP, GROUP_ID, GROUP_NAME));
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        db.execSQL("drop table if exists " + TABLE_GROUPS);
+        db.execSQL(String.format("drop table if exists %s", TABLE_BUILDING));
+        db.execSQL(String.format("drop table if exists %s", TABLE_ROOM));
+        db.execSQL(String.format("drop table if exists %s", TABLE_LESSON));
+        db.execSQL(String.format("drop table if exists %s", TABLE_DAY));
+        db.execSQL(String.format("drop table if exists %s", TABLE_TEACHERS));
+        db.execSQL(String.format("drop table if exists %s", TABLE_DISCIPLINE));
+        db.execSQL(String.format("drop table if exists %s", TABLE_GROUP_DISC_RELATIONS));
+        db.execSQL(String.format("drop table if exists %s", TABLE_TABLING));
+        db.execSQL(String.format("drop table if exists %s", TABLE_TEACH_DISC_RELATIONS));
+        db.execSQL(String.format("drop table if exists %s", TABLE_GROUP));
         onCreate(db);
     }
 }
